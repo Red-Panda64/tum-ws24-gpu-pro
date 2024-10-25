ad931ff03d45bfde6908595a9958cce5cfb16531
Fabian Stemmler <stemmler@in.tum.de>
2024-10-25 23:42:58 +0200

    get triangle application up and running

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,46 @@
-#include <iostream>
+#include "tga/tga.hpp"
+#include "tga/tga_utils.hpp"
+#include "tga/tga_vulkan/tga_vulkan.hpp"
+#include <filesystem>
+#include <stdlib.h>
 
-int main() {
-	std::cout << "\u25B3\n";
+int main(int argc, char *argv[])
+{
+    if (argc >= 0) {
+        std::filesystem::path executable{argv[0]};
+        std::filesystem::current_path(executable.parent_path());
+    }
+    // Open the interface
+    tga::Interface tgai{};
+
+    // Use utility function to load Shader from File
+    tga::Shader vertexShader = tga::loadShader("./shaders/triangle_vert.spv", tga::ShaderType::vertex, tgai);
+    tga::Shader fragmentShader = tga::loadShader("./shaders/triangle_frag.spv", tga::ShaderType::fragment, tgai);
+
+    // Get screen Resolution, use structured bindings to unpack std::pair
+    auto [screenResX, screenResY] = tgai.screenResolution();
+
+    // Window with the size of the screen
+    tga::Window window = tgai.createWindow({screenResX * 3 / 4, screenResY * 3 / 4});
+    tgai.setWindowTitle(window, "\u25B3");
+
+    // Renderpass using the shaders and rendering to the window
+    tga::RenderPass renderPass = tgai.createRenderPass(
+        tga::RenderPassInfo{vertexShader, fragmentShader, window}.setClearOperations(tga::ClearOperation::color));
+
+    // Single CommandBuffer that will be reused every frame
+    tga::CommandBuffer cmdBuffer{};
+
+    while (!tgai.windowShouldClose(window)) {
+        auto nextFrame = tgai.nextFrame(window);
+        cmdBuffer = tga::CommandRecorder{tgai, cmdBuffer}
+                        .setRenderPass(renderPass, nextFrame, {0.13, 0.13, 0.13, 1.})
+                        .draw(3, 0)
+                        .endRecording();
+
+        // Execute commands and show the result
+        tgai.execute(cmdBuffer);
+        tgai.present(window, nextFrame);
+    }
+    return 0;
 }
