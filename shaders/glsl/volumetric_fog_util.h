b1b763554a66f3344e120eacf6dacee6c670be73
omer-kose <omerkose138@gmail.com>
2025-01-10 23:41:53 +0100

    wip fog light: shader compiles. We have validation erros for dispatch during a render pass and something about image layouts

--- shaders/glsl/volumetric_fog_util.h
+++ shaders/glsl/volumetric_fog_util.h
@@ -31,10 +31,12 @@
 #define VOLUME_HEIGHT 256.0
 #define VOLUME_DEPTH  128.0
 
+#define PI 3.14159265358979323846
+
 vec3 ndcFromThreadID(uvec3 threadID)
 {
     // Map from thread id to the Vulkan NDC range X: [-1,1], Y: [-1,1], Z: [0,1]
-    return ((vec3)threadID.xyz) * vec3(2.0f / VOLUME_WIDTH, 2.0f / VOLUME_HEIGHT, 1.0f / VOLUME_DEPTH) + vec3(-1, -1, 0); 
+    return (vec3(threadID.xyz)) * vec3(2.0f / VOLUME_WIDTH, 2.0f / VOLUME_HEIGHT, 1.0f / VOLUME_DEPTH) + vec3(-1, -1, 0); 
 }
 
 vec3 ndcFromThreadID(vec3 threadID)
@@ -78,7 +80,7 @@ float depthToVolumeZPos(float depth)
 
 vec3 volumeTextureSpaceFromNdc(vec3 volumeSS)
 {
-    return saturate(volumeSS) * vec3((VOLUME_WIDTH - 1) / VOLUME_WIDTH, (VOLUME_HEIGHT - 1) / VOLUME_HEIGHT, (VOLUME_DEPTH - 1) / VOLUME_DEPTH) + vec3(0.5f / VOLUME_WIDTH, 0.5f / VOLUME_HEIGHT, 0.5f / VOLUME_DEPTH);
+    return clamp(volumeSS, 0.0, 1.0) * vec3((VOLUME_WIDTH - 1) / VOLUME_WIDTH, (VOLUME_HEIGHT - 1) / VOLUME_HEIGHT, (VOLUME_DEPTH - 1) / VOLUME_DEPTH) + vec3(0.5f / VOLUME_WIDTH, 0.5f / VOLUME_HEIGHT, 0.5f / VOLUME_DEPTH);
 }
 
 float getPhaseFunction(in float cosPhi, float gFactor)
@@ -87,9 +89,9 @@ float getPhaseFunction(in float cosPhi, float gFactor)
     return (1 - gFactor2) / pow(abs(1 + gFactor2 - 2 * gFactor * cosPhi), 1.5f) * (1.0f / 4.0f * PI);
 }
 
-float4 getRotatedPhaseFunctionSH(vec3 dir, float g)
+vec4 getRotatedPhaseFunctionSH(vec3 dir, float g)
 {
     // Returns properly rotated spherical harmonics (from Henyey-Greenstein Zonal SH expansion) 
     // for given view vector direction.
-    return float4(1.0f, dir.y, dir.z, dir.x) * float4(1.0f, g, g, g);
+    return vec4(1.0f, dir.y, dir.z, dir.x) * vec4(1.0f, g, g, g);
 }
