bd4576190b2a841cbc78c487a72b539f72085db6
omer-kose <omerkose138@gmail.com>
2025-01-14 23:16:15 +0100

    PBR Shading is implemented. I feel like normal mapping is a bit off. Triangles are clearly visible in the brass mesh.

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -22,15 +22,26 @@ tga::Texture loadTex(tga::Interface& tgai, const std::string& file)
 Mesh::Mesh(tga::Interface& tgai, const char* obj, const tga::VertexLayout& vertexLayout)
 {
     std::filesystem::path objPath = obj;
-    std::string diffuseTexturePath = objPath.replace_extension().string();
-    std::string specularTexturePath = diffuseTexturePath;
-    diffuseTexturePath.append("_diffuse.png");
-    specularTexturePath.append("_specular.png");
+    std::string texturesPath = objPath.replace_extension().string();
+    std::string albedoTexturePath = texturesPath + std::string("_albedo.png");
+    std::string normalTexturePath = texturesPath + std::string("_normal.png");
+    std::string metallicTexturePath = texturesPath + std::string("_metal.png");
+    std::string roughnessTexturePath = texturesPath + std::string("_roughness.png");
+    std::string aoTextureMap = texturesPath + std::string("_ao.png");
     tga::Obj loadedObj = tga::loadObj(obj);
     verticesArray = loadedObj.vertexBuffer;
     indicesArray = loadedObj.indexBuffer;
     // Load the textures
-    diffuseTexture = loadTex(tgai, diffuseTexturePath);
-    specularTexture = loadTex(tgai, specularTexturePath);
+    albedoMap = loadTex(tgai, albedoTexturePath);
+    normalMap = loadTex(tgai, normalTexturePath);
+    metallicMap = loadTex(tgai, metallicTexturePath);
+    roughnessMap = loadTex(tgai, roughnessTexturePath);
+    aoMap = loadTex(tgai, aoTextureMap);
+}
+
+tga::InputSet Mesh::getTextureInputSet(tga::Interface& tgai, const tga::RenderPass rp) const
+{
+    // Textures are on set 1 (hardcoded for the time being)
+    return tgai.createInputSet({ rp, { tga::Binding(albedoMap, 0, 0), tga::Binding(normalMap, 1, 0), tga::Binding(metallicMap, 2, 0), tga::Binding(roughnessMap, 3, 0), tga::Binding(aoMap, 4, 0) }, 1 });
 }
 
