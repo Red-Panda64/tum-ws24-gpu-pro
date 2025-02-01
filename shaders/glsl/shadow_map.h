3bd07b434dadd6308183d05421cf170e45d2c46c
Fabian Stemmler <stemmler@in.tum.de>
2025-02-01 21:46:21 +0100

    2x2 pcf with bilinear filtering

--- shaders/glsl/shadow_map.h
+++ shaders/glsl/shadow_map.h
@@ -1,6 +1,3 @@
-#define PCF_U_SAMPLES 4
-#define PCF_V_SAMPLES 4
-
 // can't use shadow sampler unfortunately, TGA does not expose the depth textures
 float getShadowValue(mat4 lightPV, sampler2D shadowMap, vec4 worldPosition, float bias) {
     // TODO: slope dependent bias
@@ -10,19 +7,10 @@ float getShadowValue(mat4 lightPV, sampler2D shadowMap, vec4 worldPosition, floa
         return 1.0f;
     }
     vec2 shadowUVs = lightspacePosition.xy * 0.5f + vec2(0.5f);
-    float smSample = 0.0f;
-    vec2 offsetRange = vec2(1.0f, 1.0f) / textureSize(shadowMap, 0);
-    for(int i = 0; i < PCF_U_SAMPLES; ++i) {
-        for(int j = 0; j < PCF_V_SAMPLES; ++j) {
-            vec2 offsetUVs = vec2(mix(-offsetRange.x, offsetRange.x, float(i) / float(PCF_U_SAMPLES - 1)), mix(-offsetRange.y, offsetRange.y, float(j) / float(PCF_V_SAMPLES - 1)));
-            offsetUVs = shadowUVs + offsetUVs;
-            if(any(lessThan(offsetUVs, vec2(0.0f, 0.0f))) || any(greaterThan(offsetUVs, vec2(1.0f, 1.0f)))) {
-                // TGA won't let me change texture border color, without messing around in its internals
-                smSample += 1.0f;
-            } else {
-                smSample += (texture(shadowMap, offsetUVs).r > lightspacePosition.z - bias) ? 1.0f : 0.0f;
-            }
-        }
-    }
-    return smSample / float(PCF_U_SAMPLES * PCF_V_SAMPLES);
+    vec2 texelIdx = shadowUVs * textureSize(shadowMap, 0);
+    vec2 texelFract = fract(texelIdx);
+    shadowUVs = floor(texelIdx) / textureSize(shadowMap, 0);
+    vec4 pcfSamples = textureGather(shadowMap, shadowUVs);
+    vec4 testResults = vec4(greaterThan(pcfSamples, vec4(lightspacePosition.z - bias)));
+    return mix(mix(testResults.x, testResults.y, texelFract.x), mix(testResults.w, testResults.z, texelFract.x), 1.0 - texelFract.y);
 }
